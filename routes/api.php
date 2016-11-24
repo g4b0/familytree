ef0aa996daa2683c5f355306e22a15652e70d2a3
g4b0 <gabriele@brosulo.net>
2016-11-24 21:12:27 +0100

    Added CRUD skeleton for people

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+/**
+ * CRUD routing
+ */
+Route::resource('people', 'PersonController');
